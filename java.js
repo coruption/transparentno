cbe84f7d5afcdf206054d5974889379ef2bcb4ae
Ante Malenica <antemalenica@gmail.com>
2014-07-31 21:58:38 +0200

    dodana podstranica i nekoliko većih izmjena

---
+++ java.js
@@ -0,0 +1,24 @@
+
+    function Clear(input){
+             if (input.value=="Upišite ime ili OIB poslovnog subjekta"){
+                document.getElementById('oib').innerHTML="Upišite ime ili OIB poslovnog subjekta";            }
+            else{
+                
+                document.getElementById('oib').innerHTML="";
+            }
+        }
+  
+     function SetPh (input) {
+           // body...
+           if (input.value==""){
+            document.getElementById('oib').innerHTML="Upišite ime ili OIB poslovnog subjekta";
+           }
+           else{
+            document.getElementById('oib').innerHTML="";
+           }
+       }  
+        
+        
+        
+
+    
